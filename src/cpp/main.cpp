de660d77bcc8e0cb5ed7fbee998a574ee73b2a3b
Paul Joh <pauljoh@gmx.de>
2023-11-12 11:23:04 +0100

    commitmsg

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -12,7 +12,7 @@ inline std::string get_path_to_Secihurd_Model()
     size_t pos = 0;
     while ((pos = current_working_dir.find_last_of("\\")) != std::string::npos) {
         std::string folder = current_working_dir.substr(pos + 1, current_working_dir.size() - 1);
-        if (folder != "Secihurd_Model") {
+        if (folder != "ReproductionNbSim") {
             current_working_dir.erase(pos, current_working_dir.size() - 1);
         }
         else {
